cf8a22424f2d309899a0a03cd065956b10bf0d73
diogosalmeida <diogomean@gmail.com>
2017-08-11 18:57:48 -0300

    Usando o metodo Object.assign para melhorar a leitura do projeto

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,7 +1,25 @@
 class Negociacao {
-    constructor(data, quantidade, valor){
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+    constructor(data, quantidade, valor) {
+        Object.assign(this,{
+            _data: new Date(data.getTime())
+            , _quantidade: quantidade
+            , _valor: valor
+        });
+
+        Object.freeze(this);
+
+    }
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+
+    get data() {
+        return new Date(this._data.getTime());
+    }
+    get quantidade() {
+        return this._quantidade;
+    }
+    get valor() {
+        return this._valor;
     }
 }
