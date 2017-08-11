bb0e4c0fe21bd030ac551c452a88205f5f60847b
diogosalmeida <diogomean@gmail.com>
2017-08-11 18:32:54 -0300

    remoção de códigos sem ultilização

--- client/app/index.js
+++ client/app/index.js
@@ -3,20 +3,34 @@ var campos = [
     document.querySelector('#valor'),
     document.querySelector('#quantidade')
 ];
-console.log(campos);	//	verificando	o	conteúdo	do	array
+
+
 var tbody = document.querySelector('table	tbody');
-document.querySelector('.form').addEventListener('submit', function (event) {
+document.querySelector('.form').addEventListener('submit',(event) => {
 
     event.preventDefault()
     var tr = document.createElement('tr');
-    campos.forEach(function (campo) {
+    campos.forEach((campo) => {
         var td = document.createElement('td');
         td.textContent = campo.value;
         tr.appendChild(td);
     });
+
     var tdVolume = document.createElement('td');
+
     tdVolume.textContent = campos[1].value * campos[2].value;
+
     tr.appendChild(tdVolume);
 
     tbody.appendChild(tr)
+
+    	//	limpa	o	campo	da	data
+        campos[0].value	=	'';
+        //	limpa	o	campo	da	quantidade
+        campos[1].value	=	1;
+        //	limpa	o	campo	do	valor
+        campos[2].value	=	0;
+        //	foca	no	campo	da	data
+        campos[0].focus();
 });
+
